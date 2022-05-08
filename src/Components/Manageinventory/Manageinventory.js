98dfc33c1382807d3cd5b594e69a3fed535a6449
unknown <ayt.ullah.dev@gmail.com>
2022-05-08 06:34:33 +0600

    Curbtn active pagination addded

--- src/Components/Manageinventory/Manageinventory.js
+++ src/Components/Manageinventory/Manageinventory.js
@@ -8,8 +8,10 @@ const Manageinventory = () => {
   const [limit, setLimit] = useState(5);
   const [pages, setPages] = useState(0);
   const [totalpro, setTotalpro] = useState(0);
-  const [curpage,setCurpage] = useState(0);
-  const [url,setUrl] =useState(`https://young-beach-37066.herokuapp.com/products?limit=${limit}&page=${curpage}`)
+  const [curpage, setCurpage] = useState(0);
+  const [url, setUrl] = useState(
+    `https://young-beach-37066.herokuapp.com/products?limit=${limit}&page=${curpage}`
+  );
   useEffect(() => {
     fetch("https://young-beach-37066.herokuapp.com/productcount/", {
       headers: {
@@ -23,7 +25,7 @@ const Manageinventory = () => {
         setPages(Math.ceil(data.count / limit));
       });
   }, []);
-  
+
   return (
     <>
       <div className="flex items-center justify-center space-x-4 py-3">
@@ -48,14 +50,22 @@ const Manageinventory = () => {
         {!showmpro && <Addproduct />}
         {showmpro && (
           <div className=" min-h-screen">
-            <Manaageallproduct
-              url={url}
-            />
-            <div className="px-5">
-                {
-                  [...Array(pages).keys()]
-                  .map(num=>(<button className="btn ml-1" key={num} onClick={()=>{setUrl(`https://young-beach-37066.herokuapp.com/products?limit=${limit}&page=${num}`)}}>{num}</button>))
-                }
+            <Manaageallproduct url={url} />
+            <div className="px-5 btn-group">
+              {[...Array(pages).keys()].map((num) => (
+                <button
+                  className={`btn ml-1 ${num === curpage ? "btn-active" : ""}`}
+                  key={num}
+                  onClick={() => {
+                    setCurpage(num);
+                    setUrl(
+                      `https://young-beach-37066.herokuapp.com/products?limit=${limit}&page=${num}`
+                    );
+                  }}
+                >
+                  {num}
+                </button>
+              ))}
             </div>
           </div>
         )}
--- src/Components/Manageinventory/Myitems.js
+++ src/Components/Manageinventory/Myitems.js
@@ -1,39 +1,54 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../Firebase/firebase.init';
-import Manaageallproduct from './Manaageallproduct';
-import { useState,useEffect } from 'react';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase/firebase.init";
+import Manaageallproduct from "./Manaageallproduct";
+import { useState, useEffect } from "react";
 const Myitems = () => {
-    const [user]=useAuthState(auth);
-    const [limit, setLimit] = useState(5);
+  const [user] = useAuthState(auth);
+  const [limit, setLimit] = useState(5);
   const [pages, setPages] = useState(0);
   const [totalpro, setTotalpro] = useState(0);
-  const [curpage,setCurpage] = useState(0);
-  const [url,setUrl] =useState(`https://young-beach-37066.herokuapp.com/productsby/?email=${user.email}&limit=${limit}&page=${curpage}`)
+  const [curpage, setCurpage] = useState(0);
+  const [url, setUrl] = useState(
+    `https://young-beach-37066.herokuapp.com/productsby/?email=${user.email}&limit=${limit}&page=${curpage}`
+  );
   useEffect(() => {
-    fetch(`https://young-beach-37066.herokuapp.com/productcount/?email=${user.email}`, {
-      headers: {
-        "content-type": "application/json",
-        authorization: `bearear ${localStorage.getItem("token")}`,
-      },
-    })
+    fetch(
+      `https://young-beach-37066.herokuapp.com/productcount/?email=${user.email}`,
+      {
+        headers: {
+          "content-type": "application/json",
+          authorization: `bearear ${localStorage.getItem("token")}`,
+        },
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         setTotalpro(data.count);
         setPages(Math.ceil(data.count / limit));
       });
   }, []);
-    return (
-        <div className=" min-h-screen">
-            <Manaageallproduct url={url}/>
-            <div className="px-5">
-                {
-                  [...Array(pages).keys()]
-                  .map(num=>(<button className="btn ml-1" key={num} onClick={()=>{setUrl(`https://young-beach-37066.herokuapp.com/productsby/?email=${user.email}&limit=${limit}&page=${num}`)}}>{num+1}</button>))
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className=" min-h-screen">
+      <Manaageallproduct url={url} />
+      <div className="px-5 btn-group">
+        {[...Array(pages).keys()].map((num) => (
+          <button
+            className={`btn ml-1 ${num === curpage ? "btn-active" : ""}`}
+            key={num}
+            onClick={() => {
+                setCurpage(num);
+              setUrl(
+                `https://young-beach-37066.herokuapp.com/productsby/?email=${user.email}&limit=${limit}&page=${num}`
+              );
+            }}
+          >
+            {num + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Myitems;
